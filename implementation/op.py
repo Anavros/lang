665969caaa1d1096dcf17c6b57636d6f167c38ec
Anavros <jtdobbs1@me.com>
2017-02-06 22:17:17 -0500

    Basic interpreter in python. Also renamed folder.

---
+++ implementation/op.py
@@ -0,0 +1,12 @@
+
+
+def op_print(args):
+    print(args)
+
+
+def op_assign(args):
+    pass
+
+
+def op_exit(args):
+    pass
--- implementation/parser.py
+++ implementation/parser.py
@@ -1,6 +1,7 @@
 
 from string import ascii_letters, digits, whitespace
 from tools import Processor, chars
+from op import *
 
 LPAREN = '('
 LBRACE = '{'
@@ -53,13 +54,62 @@ def extract_functions(tokens):
     return funcs
 
 
+def extract_assignments(tokens):
+    assign = {};
+    key = ""
+    valtrap = False
+    for t in tokens:
+        if valtrap:
+            assign[key] = t
+            valtrap = False
+        elif set(t) <= set(KEYWORD):
+            key = t
+        elif t == '=':
+            assign[key] = None
+            valtrap = True
+    return assign
+
+
+def match_functions_with_arguments(tokens):
+    """
+    ['print', '(', ')'] => execute('print', [])
+    ['print', '(', '10', ')'] => execute('print', [10])
+    ['(', 'print', ')'] => error or expression?
+    """
+    func = ""
+    args = []
+    parens = 0
+    for t in tokens:
+        if set(t) <= set(KEYWORD):
+            if parens:
+                args.append(t)
+            else:
+                func = t
+        elif t == LPAREN:
+            parens += 1
+        elif t == RPAREN:
+            parens -= 1
+    return func, args
+
+
+def execute(operations):
+    for f, args in operations:
+        if f == 'print':
+            op_print(args)
+
+
 def main(args):
     f = open(args.filename)
     ments = break_into_statements(f)
     exprs = [break_into_tokens(s) for s in ments]
-    funcs = list(map(extract_functions, exprs))
-    print(exprs)
-    print(funcs)
+    #funcs = list(map(extract_functions, exprs))
+    #assgn = list(map(extract_assignments, exprs))
+    mpped = list(map(match_functions_with_arguments, exprs))
+    execute(mpped)
+    #print(exprs)
+    #print(funcs)
+    #print(assgn)
+    #print(mpped)
     f.close()
 
 
